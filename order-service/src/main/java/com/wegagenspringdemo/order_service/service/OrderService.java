8e8990c7fcd4d95744d4443d88090822793242a8
seifeda <seifebekele07@gmail.com>
2024-09-30 21:38:22 +0300

    finishing discovery Server

--- order-service/src/main/java/com/wegagenspringdemo/order_service/service/OrderService.java
+++ order-service/src/main/java/com/wegagenspringdemo/order_service/service/OrderService.java
@@ -24,7 +24,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -43,8 +43,9 @@ public class OrderService {
                 .toList();
         // Call Inventory Service, and place order if product is in
         // stock
-        InventoryResponse[] inventoryResponsesArray=webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponsesArray=webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                       // .uri("http://localhost:8082/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                         .retrieve()
                                 .bodyToMono(InventoryResponse[].class)
